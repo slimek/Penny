7de5952070942569fe0f132184b5e66153477585
Slimek Wu <slimek.wu@nxtomo.tw>
2017-05-12 18:49:37 +0800

    Add Guzzle HTTP Client and /ip/ip-info

--- src/Controllers/IpController.php
+++ src/Controllers/IpController.php
@@ -2,6 +2,7 @@
 namespace Controllers;
 
 // Composer
+use GuzzleHttp\Client;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -15,5 +16,23 @@ class IpController
         ];
 
         return $response->withJson($result);
+    }
+
+    // 查詢 request 的來源 IP 資訊
+    public function ipInfo(Request $request, Response $response)
+    {
+        $ipAddress = $request->getAttribute('ip_address');
+
+        $client = new Client(['base_uri' => 'https://ipinfo.io/']);
+        $res = $client->request('GET', $ipAddress . '/json');
+
+        if ($res->getStatusCode() != 200) {
+            throw new \Exception(
+                'Call to ipinfo.io failed, status code: ' . $res->getStatusCode()
+            );
+        }
+
+        $ipInfo = json_decode($res->getBody());
+       return $response->withJson($ipInfo);
     }
 }
