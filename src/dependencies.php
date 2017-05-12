0ddb10ff407d2624512c453cc609ff8ddbcec9b3
Slimek Wu <slimek.wu@nxtomo.tw>
2017-05-12 13:04:23 +0800

    Add Monolog package

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,1 +1,26 @@
 <?php
+
+//======================================================================================================================
+// Dependency Injection
+//======================================================================================================================
+
+$container = $app->getContainer();
+
+//----------------------------------------------------------------------------------------------------------------------
+// Logging
+//----------------------------------------------------------------------------------------------------------------------
+
+$container['logger'] = function ($container) {
+
+    $logger = new Monolog\Logger('penny');
+
+    // 1. 路徑是相對於 index.php 所在的 public 目錄
+    // 2. 利用 %F 加入日期標記
+    $path = strftime('../log/penny_%F.log');
+
+    $handler = new Monolog\Handler\StreamHandler($path);
+    $handler->setFormatter(new Monolog\Formatter\LineFormatter(null, null, true, true));
+    $logger->pushHandler($handler);
+
+    return $logger;
+};
