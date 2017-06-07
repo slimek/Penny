251255ab734438f8bc6b673e83b60a3594a0692b
Slimek Wu <slimek.wu@nxtomo.tw>
2017-06-07 16:17:25 +0800

    Remove Monolog StreamHandler
    - All outputs are passed to Fluentd.

--- src/dependencies.php
+++ src/dependencies.php
@@ -14,14 +14,7 @@ $container['logger'] = function ($container) {
 
     $logger = new Monolog\Logger('penny');
 
-    // 1. 路徑是相對於 index.php 所在的 public 目錄
-    // 2. 利用 %F 加入日期標記
-    $path = strftime('../log/penny_%F.log');
-
-    $handler = new Monolog\Handler\StreamHandler($path);
-    $handler->setFormatter(new Monolog\Formatter\LineFormatter(null, null, true, true));
-    $logger->pushHandler($handler);
-
+    // 透過 Fluentd 輸出 log
     $handler = new Dakatsuka\MonologFluentHandler\FluentHandler(
         null,
         $container->settings['fluentdHost']
